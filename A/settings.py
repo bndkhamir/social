ac094f5d8b5708d4238226f372dd9031e3025cd1
Ramin <raminmoradi3690@gmail.com>
2022-04-10 20:57:57 +0430

    add google confige for send rest email

--- A/settings.py
+++ A/settings.py
@@ -135,3 +135,13 @@ AUTHENTICATION_BACKENDS =[
 ]
 
 LOGIN_URL = '/accounts/Login/'
+
+#google_Account
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bndkhmir@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD ='zhveyqcdhtrrtfbh'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Ramin&Maede'
